075cc9897b9bb8b25776ea6d3146f74d74b05476
Nathan Robinson <nrobinson2000@me.com>
2020-10-13 04:56:41 -0400

    fix setup.sh at 04:56 of 2020-10-13

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
+#!/bin/bash
+
+# setup.sh
+# Script for replicating my Manjaro Cinnamon setup
+
+# Install Packages
 sudo pacman-mirrors --country United_States
 sudo pacman -Syyu --noconfirm base-devel yay vim plank adapta-gtk-theme albert muparser
 
-
+# Copy autostart apps
 APPS="$(find apps/ | tail +2)"
-
-for app in "$APPS"; do
+for app in $APPS; do
     cp "$app" "$HOME/.config/autostart"
 done
 
+# Copy dotfiles
 DOTFILES="$(find dotfiles/ | tail +2)"
-
-for file in "$DOTFILES"; do
+for file in $DOTFILES; do
     cp "$file" "$HOME"
 done
