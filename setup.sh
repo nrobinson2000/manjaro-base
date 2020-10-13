1263298541d5666ec0ddf4700e23ae474c983806
Nathan Robinson <nrobinson2000@me.com>
2020-10-13 04:49:22 -0400

    at 04:49 of 2020-10-13

--- setup.sh
+++ setup.sh
@@ -1,2 +1,15 @@
 sudo pacman-mirrors --country United_States
-sudo pacman -Syyu --noconfirm base-devel yay vim plank adapta-gtk-theme albert
+sudo pacman -Syyu --noconfirm base-devel yay vim plank adapta-gtk-theme albert muparser
+
+
+APPS="$(find apps/ | tail +2)"
+
+for app in "$APPS"; do
+    cp "$app" "$HOME/.config/autostart"
+done
+
+DOTFILES="$(find dotfiles/ | tail +2)"
+
+for file in "$DOTFILES"; do
+    cp "$file" "$HOME"
+done
