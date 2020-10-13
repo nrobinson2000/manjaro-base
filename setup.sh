c37b87abd52987f49bb87d1c19b12d29e8fc998c
Nathan Robinson <nrobinson2000@me.com>
2020-10-13 12:56:24 -0400

    update script at 12:56 of 2020-10-13

--- setup.sh
+++ setup.sh
@@ -3,25 +3,25 @@
 # setup.sh
 # Script for replicating my Manjaro Cinnamon setup
 
-# Install Packages
+echo "Install Packages"
 sudo pacman-mirrors --country United_States
 sudo pacman -Syyu --noconfirm base-devel yay vim plank adapta-gtk-theme albert muparser chromium
 
-# Copy autostart apps
+echo "Copy autostart apps"
 mkdir -p "$HOME/.config/autostart"
 APPS="$(find apps/ | tail +2)"
 for app in $APPS; do
     cp "$app" "$HOME/.config/autostart"
 done
 
-# Copy dotfiles
+echo "Copy dotfiles"
 DOTFILES="$(find dotfiles/ | tail +2)"
 for file in $DOTFILES; do
     cp "$file" "$HOME"
 done
 
-# Apply dconf settings
-dconf load / < manjaro-blade.conf
+echo "Apply dconf settings"
+dconf load / < "conf/manjaro-blade.conf"
 
-# Copy albert.conf
-install -D albert.conf "$HOME/.config/albert/albert.conf"
+echo "Copy albert.conf"
+install -D "conf/albert.conf" "$HOME/.config/albert/albert.conf"
